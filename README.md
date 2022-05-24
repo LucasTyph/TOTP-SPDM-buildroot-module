db316abdb2579b23f0ff6df4fc318972ee21fa96
Lucas de Menezes <lucasdemenezes@larc.usp.br>
2022-05-24 12:50:13 -0300

    renamed main file, added timer based function

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ requires setting the following variables on Buildroot:
 - `BR2_EXTERNAL`  to this directory (e.g. `BR2_EXTERNAL=/home/user/this_repo/`)
 - `BR2_ROOTFS_OVERLAY` to the `rootfs-overlay` directory in this repository (e.g. `BR2_ROOTFS_OVERLAY=$(BR2_EXTERNAL_TOTP_SPDM_PATH)/rootfs-overlay`
   -  alternatively, during `menuconfig`, set System configurations -> Root filesystem overlay directories to `$(BR2_EXTERNAL_TOTP_SPDM_PATH)/rootfs-overlay`
+
+any new or renamed .c files must be added to the `S16totp-spdm` script in order to be initialized during startup
--- totp_spdm_driver.c
+++ totp_spdm_driver.c
@@ -2,11 +2,13 @@
 #include <linux/interrupt.h>
 #include <linux/module.h>
 #include <linux/usb.h>
+#include <linux/hrtimer.h>
+#include <linux/sched.h>
 
 /*
-* This macro is used to tell the driver to use old method or new method.
-*  If it is 0, then driver will use old method. ie: __init and __exit
-*  If it is non zero, then driver will use new method. ie: module_usb_driver
+** This macro is used to tell the driver to use old method or new method.
+** If it is 0, then driver will use old method. ie: __init and __exit
+** If it is non zero, then driver will use new method. ie: module_usb_driver
 */
 #define IS_NEW_METHOD_USED (1)
 #define USB_VENDOR_ID (0x0666)
@@ -14,30 +16,58 @@
 #define IRQ_NO 11
 
 static void print_usb_interface_descriptor (const struct usb_interface_descriptor i) {
-    pr_info("USB_INTERFACE_DESCRIPTOR:\n");
-    pr_info("-----------------------------\n");
-    pr_info("bLength: 0x%x\n", i.bLength);
-    pr_info("bDescriptorType: 0x%x\n", i.bDescriptorType);
-    pr_info("bInterfaceNumber: 0x%x\n", i.bInterfaceNumber);
-    pr_info("bAlternateSetting: 0x%x\n", i.bAlternateSetting);
-    pr_info("bNumEndpoints: 0x%x\n", i.bNumEndpoints);
-    pr_info("bInterfaceClass: 0x%x\n", i.bInterfaceClass);
-    pr_info("bInterfaceSubClass: 0x%x\n", i.bInterfaceSubClass);
-    pr_info("bInterfaceProtocol: 0x%x\n", i.bInterfaceProtocol);
-    pr_info("iInterface: 0x%x\n", i.iInterface);
-    pr_info("\n");
+	pr_info("USB_INTERFACE_DESCRIPTOR:\n");
+	pr_info("-----------------------------\n");
+	pr_info("bLength: 0x%x\n", i.bLength);
+	pr_info("bDescriptorType: 0x%x\n", i.bDescriptorType);
+	pr_info("bInterfaceNumber: 0x%x\n", i.bInterfaceNumber);
+	pr_info("bAlternateSetting: 0x%x\n", i.bAlternateSetting);
+	pr_info("bNumEndpoints: 0x%x\n", i.bNumEndpoints);
+	pr_info("bInterfaceClass: 0x%x\n", i.bInterfaceClass);
+	pr_info("bInterfaceSubClass: 0x%x\n", i.bInterfaceSubClass);
+	pr_info("bInterfaceProtocol: 0x%x\n", i.bInterfaceProtocol);
+	pr_info("iInterface: 0x%x\n", i.iInterface);
+	pr_info("\n");
 }
  
 static void print_usb_endpoint_descriptor (const struct usb_endpoint_descriptor e) {
-    pr_info("USB_ENDPOINT_DESCRIPTOR:\n");
-    pr_info("------------------------\n");
-    pr_info("bLength: 0x%x\n", e.bLength);
-    pr_info("bDescriptorType: 0x%x\n", e.bDescriptorType);
-    pr_info("bEndPointAddress: 0x%x\n", e.bEndpointAddress);
-    pr_info("bmAttributes: 0x%x\n", e.bmAttributes);
-    pr_info("wMaxPacketSize: 0x%x\n", e.wMaxPacketSize);
-    pr_info("bInterval: 0x%x\n", e.bInterval);
-    pr_info("\n");
+	pr_info("USB_ENDPOINT_DESCRIPTOR:\n");
+	pr_info("------------------------\n");
+	pr_info("bLength: 0x%x\n", e.bLength);
+	pr_info("bDescriptorType: 0x%x\n", e.bDescriptorType);
+	pr_info("bEndPointAddress: 0x%x\n", e.bEndpointAddress);
+	pr_info("bmAttributes: 0x%x\n", e.bmAttributes);
+	pr_info("wMaxPacketSize: 0x%x\n", e.wMaxPacketSize);
+	pr_info("bInterval: 0x%x\n", e.bInterval);
+	pr_info("\n");
+}
+
+static struct hrtimer htimer;
+static ktime_t kt_period;
+
+static enum hrtimer_restart timer_function(struct hrtimer *timer)
+{
+	// @Do your work here.
+	printk(KERN_INFO "timer_function\n");
+
+	hrtimer_forward_now(timer, kt_period);
+	return HRTIMER_RESTART;
+}
+
+static void timer_init(void) {
+	kt_period = ktime_set(10, 0); // seconds, nanoseconds
+	hrtimer_init(&htimer, CLOCK_REALTIME, HRTIMER_MODE_REL);
+	htimer.function = timer_function;
+	hrtimer_start(&htimer, kt_period, HRTIMER_MODE_REL);
+}
+
+static void timer_cleanup(void) {
+	hrtimer_cancel(&htimer);
+}
+
+static irqreturn_t sample_irq(int irq, void *dev_id){
+	printk("irq %d\n", irq);
+	return IRQ_RETVAL(1);
 }
 
 /*
@@ -56,9 +86,12 @@ static int etx_usb_probe (struct usb_interface *interface, const struct usb_devi
 
 	print_usb_interface_descriptor(iface_desc->desc);
 
-	for ( i = 0; i < endpoints_count; i++ ) {
+	for (i = 0; i < endpoints_count; i++) {
 		print_usb_endpoint_descriptor(iface_desc->endpoint[i].desc);
 	}
+	
+	printk(KERN_INFO "Initializing timer-based function\n");
+	
 	return 0;  //return 0 indicates we are managing this device
 }
 
@@ -66,8 +99,10 @@ static int etx_usb_probe (struct usb_interface *interface, const struct usb_devi
 ** This function will be called when USB device is removed.
 */
 static void etx_usb_disconnect (struct usb_interface *interface) {
-	printk(KERN_INFO "etx_usb_disconnect\n");
-	dev_info(&interface->dev, "USB Driver Disconnected\n");
+	printk (KERN_INFO "etx_usb_disconnect\n");
+	free_irq (IRQ_NO,(void *)(sample_irq));
+	timer_cleanup();
+	dev_info (&interface->dev, "USB Driver Disconnected\n");
 }
  
 //usb_device_id provides a list of different types of USB devices that the driver supports
@@ -94,6 +129,7 @@ module_usb_driver(etx_usb_driver);
 #else
 static int __init etx_usb_init (void) {
 	printk(KERN_INFO "etx_usb_init\n");
+	timer_init();
 	//register the USB device
 	return usb_register(&etx_usb_driver);
 }
@@ -112,30 +148,3 @@ MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("TOTP + SPDM USB Driver");
 MODULE_VERSION("0.2");
 
-// código de interrupções
-/*
-static irqreturn_t sample_irq(int irq, void *dev_id){
-	printk("irq %d\n", irq);
-	return IRQ_RETVAL(1);
-}
-
-static int myinit(void) {
-	printk(KERN_INFO "hello init\n");
-	int ret;
-	
-	ret = request_irq(IRQ_NO, &sample_irq, IRQF_SHARED, "uhci_hcd:usb1", 0);
-	if (ret < 0) {
-		printk(KERN_ALERT "%s: request_irg failed with %d\n", __func__, ret);
-	}
-	
-	return 0;
-}
-
-static void myexit(void) {
-    printk(KERN_INFO "hello exit\n");
-}
-
-MODULE_LICENSE("GPL");
-module_init(myinit)
-module_exit(myexit)
-*/
